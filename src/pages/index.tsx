52edd4c0a1be0ff230577ff585255bdebc3f0971
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-05-17 19:36:24 +0800

    feat: add mouse wheel scroll horizontal to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { createRef } from "react";
 import { SideNav } from "../components/SideNav";
 import { HomeScreen } from "../screens/Home";
 import { PortfolioScreen } from "../screens/Portfolio";
 
 const Home: NextPage = () => {
+  const mainDivRef = createRef<HTMLDivElement>();
+
+  const scrollFunction = (scrollOffset: number) => {
+    mainDivRef.current!.scrollLeft += scrollOffset;
+  };
+
   return (
     <div>
       <Head>
@@ -15,7 +22,14 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="main">
+
+      <main
+        ref={mainDivRef}
+        className="main"
+        onWheel={(e) => {
+          scrollFunction(e.deltaY);
+        }}
+      >
         <SideNav />
         <HomeScreen />
         <PortfolioScreen />
--- src/screens/Portfolio/index.tsx
+++ src/screens/Portfolio/index.tsx
@@ -1,9 +1,8 @@
-import { FC, useState } from "react";
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
-import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 
 import { ProjectCard } from "../../components/ProjectCard";
 import { SliderControl } from "../../components/SliderControl";
@@ -25,7 +24,7 @@ export type IProject = {
   projectURL: string;
 };
 
-const PortfolioScreen: FC = () => {
+const PortfolioScreen = () => {
   const projects = projectsJson as IProject[];
   const sliderCount = projects.length;
   const [loaded, setLoaded] = useState(false);
@@ -49,49 +48,42 @@ const PortfolioScreen: FC = () => {
   });
 
   return (
-    <ReactScrollWheelHandler
-      upHandler={(e) => {
-        currentSlide !== 0 && instanceRef.current?.prev();
-      }}
-      downHandler={(e) => instanceRef.current?.next()}
-    >
-      <div className={`keen-slider ${styles.container}`}>
-        <div className={styles.textWrapper}>
-          <h5 className={styles.header}>{`Creative territory`}</h5>
-          <h1 className={styles.title}>My Lab</h1>
-        </div>
-        <div ref={ref} className={`keen-slider ${styles.body}`}>
-          {projects.map((project, idx) => (
-            <div
-              className={`keen-slider__slide ${styles.cardContainer}`}
-              key={idx}
-            >
-              <ProjectCard
-                content={project.content}
-                contentWidth={project.contentWidth}
-                imageWidth={project.imageWidth}
-                imageRight={project.imageRight}
-                imageBottom={project.imageBottom}
-                image={project.image}
-                imageW={project.imageW}
-                imageH={project.imageH}
-                title={project.title}
-                type={project.type}
-                projectURL={project.projectURL}
-              />
-            </div>
-          ))}
-        </div>
-        {loaded && instanceRef.current && (
-          <SliderControl
-            instance={instanceRef}
-            count={sliderCount}
-            currentSlide={currentSlide}
-            projects={projects}
-          />
-        )}
+    <div className={`keen-slider ${styles.container}`}>
+      <div className={styles.textWrapper}>
+        <h5 className={styles.header}>{`Creative territory`}</h5>
+        <h1 className={styles.title}>My Lab</h1>
       </div>
-    </ReactScrollWheelHandler>
+      <div ref={ref} className={`keen-slider ${styles.body}`}>
+        {projects.map((project, idx) => (
+          <div
+            className={`keen-slider__slide ${styles.cardContainer}`}
+            key={idx}
+          >
+            <ProjectCard
+              content={project.content}
+              contentWidth={project.contentWidth}
+              imageWidth={project.imageWidth}
+              imageRight={project.imageRight}
+              imageBottom={project.imageBottom}
+              image={project.image}
+              imageW={project.imageW}
+              imageH={project.imageH}
+              title={project.title}
+              type={project.type}
+              projectURL={project.projectURL}
+            />
+          </div>
+        ))}
+      </div>
+      {loaded && instanceRef.current && (
+        <SliderControl
+          instance={instanceRef}
+          count={sliderCount}
+          currentSlide={currentSlide}
+          projects={projects}
+        />
+      )}
+    </div>
   );
 };
 
