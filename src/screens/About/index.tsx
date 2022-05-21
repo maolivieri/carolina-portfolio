2d2739d50d8f0751def6c9f0eda61bdc99355337
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-05-21 18:45:25 +0800

    chore: delete unused about folder from screens folder

--- src/screens/About/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import styles from "./styles.module.scss";
-
-const AboutScreen: FC = () => {
-  return <div className={styles.container}>index</div>;
-};
-
-export { AboutScreen };
